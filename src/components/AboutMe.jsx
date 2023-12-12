92d1c14fbb82e6ef35209be5c0cdf85a9b992cd9
jasaiful <jasaiful@gmail.com>
2023-12-12 19:27:52 +0600

    added projects

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,8 +1,7 @@
 
-
 const AboutMe = () => {
     return (
-        <div name='about' className='w-full bg-base-50 h-screen pt-24'>
+        <div name='about' className='w-full h-auto h-screen pt-24'>
             <div className='max-w-6xl mx-auto p-4 flex flex-col justify-center w-full h-full'>
                 <div>
                     <p className='text-4xl font-bold inline border-b-4 border-rose-600 '>About Me</p>
@@ -10,8 +9,8 @@ const AboutMe = () => {
                         I am available for exciting opportunities to apply my passion and skills in front-end development, eager to contribute, learn, and grow in collaborative projects.
                     </p>
                 </div>
-                <div className="flex justify-between">
-                    <div className="mt-5 space-y-8">
+                <div className="md:flex flex-col md:flex-row justify-between">
+                    <div className="mt-5 space-y-8 md:w-1/2">
                         <h2 className="font-semibold mt-5 text-2xl">Personal Info:</h2>
                         <div>
                             <h4 className="text-lg border-b-2">Name:</h4>
@@ -30,8 +29,8 @@ const AboutMe = () => {
                             <p className="text-xl font-semibold">Noakhali, Bangladesh</p>
                         </div>
                     </div>
-                    <div className="md:w-1/2 p-5">
-                        <img className="hover:scale-110 w-5/12 duration-500 rounded-xl" src="https://i.ibb.co/vDJx30T/image.png" alt="banner" />
+                    <div className="md:w-1/2 p-5 md:pl-20">
+                        <img className="hover:scale-110 w-full md:w-6/12 duration-500 rounded-xl" src="https://i.ibb.co/vDJx30T/image.png" alt="banner" />
                     </div>
                 </div>
             </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,11 +3,6 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
-// import surveyMaster from '../assets/projects/Survey Master.JPG'
-// import careerCenter from '../assets/projects/Career Center.JPG'
-// import fruitBurst from '../assets/projects/Fruit Burst.JPG'
-// import courseRegistration from '../assets/projects/Course Registration.JPG'
-// import brandShop from '../assets/projects/Brand Shop.JPG'
 
 const Projects = () => {
 
@@ -35,18 +30,99 @@ const Projects = () => {
                     modules={[Autoplay, Pagination, Navigation]}
                     className="mySwiper"
                 >
+                    {/* slide-1 */}
                     <SwiperSlide>
                         <div id="slide1" className="carousel-item relative px-6 md:px-1 w-full">
                             <img className="w-full rounded-xl" src='https://i.ibb.co/fNfPFgP/Brand-Shop.jpg' alt="project_img" />
                             <div className="absolute rounded-xl flex items-center justify-end h-full hero-overlay bg-opacity-40">
                                 <div className='text-white space-y-7 text-right pr-12'>
-                                    <h2 className='text-4xl className  font-bold'>Dynamic React Application</h2>
+                                    <h2 className='text-4xl className  font-bold'>Brand Shop by React App</h2>
                                     <div>
-                                        <a href="https://github.com/jasaiful/brand-shop-client/" target="_blank" rel="noopener noreferrer">
+                                        <a href="https://github.com/jasaiful/brand-shop-client.git" target="_blank" rel="noopener noreferrer">
                                             <button className="btn btn-ghost hover:text-white outline outline-2 outline-rose-500 mr-5 text-lg hover:bg-rose-500">Code</button>
                                         </a>
 
                                         <a href="https://brand-shop-c7580.web.app/" target="_blank" rel="noopener noreferrer">
+                                            <button className="btn btn-ghost hover:text-white outline outline-2 outline-rose-500 text-lg hover:bg-rose-500">Live</button>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    {/* slide-2 */}
+                    <SwiperSlide>
+                        <div id="slide2" className="carousel-item relative px-6 md:px-1 w-full">
+                            <img className="w-full rounded-xl" src='https://i.ibb.co/kxgSwtg/Fruit-Burst.jpg' alt="project_img" />
+                            <div className="absolute rounded-xl flex items-center justify-end h-full hero-overlay bg-opacity-40">
+                                <div className='text-white space-y-7 text-right pr-12'>
+                                    <h2 className='text-4xl className  font-bold'>E-commerce Site by Tailwind CSS </h2>
+                                    <div>
+                                        <a href="https://github.com/jasaiful/A3-fruit-brust.git" target="_blank" rel="noopener noreferrer">
+                                            <button className="btn btn-ghost hover:text-white outline outline-2 outline-rose-500 mr-5 text-lg hover:bg-rose-500">Code</button>
+                                        </a>
+
+                                        <a href="https://frolicking-seahorse-8d0e1e.netlify.app/" target="_blank" rel="noopener noreferrer">
+                                            <button className="btn btn-ghost hover:text-white outline outline-2 outline-rose-500 text-lg hover:bg-rose-500">Live</button>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    {/* slide-3 */}
+                    <SwiperSlide>
+                        <div id="slide3" className="carousel-item relative px-6 md:px-1 w-full">
+                            <img className="w-full rounded-xl" src='https://i.ibb.co/SvHBhvC/Course-Registration.jpg' alt="project_img" />
+                            <div className="absolute rounded-xl flex items-center justify-end h-full hero-overlay bg-opacity-40">
+                                <div className='text-white space-y-7 text-right pr-12'>
+                                    <h2 className='text-4xl className  font-bold'>Course Registration Site by JavaScript</h2>
+                                    <div>
+                                        <a href="https://github.com/jasaiful/course-registration.git" target="_blank" rel="noopener noreferrer">
+                                            <button className="btn btn-ghost hover:text-white outline outline-2 outline-rose-500 mr-5 text-lg hover:bg-rose-500">Code</button>
+                                        </a>
+
+                                        <a href="https://fastidious-jelly-158599.netlify.app/" target="_blank" rel="noopener noreferrer">
+                                            <button className="btn btn-ghost hover:text-white outline outline-2 outline-rose-500 text-lg hover:bg-rose-500">Live</button>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    {/* slide-4 */}
+                    <SwiperSlide>
+                        <div id="slide4" className="carousel-item relative px-6 md:px-1 w-full">
+                            <img className="w-full rounded-xl" src='https://i.ibb.co/w6k3cbT/Career-Center.jpg' alt="project_img" />
+                            <div className="absolute rounded-xl flex items-center justify-end h-full hero-overlay bg-opacity-40">
+                                <div className='text-white space-y-7 text-right pr-12'>
+                                    <h2 className='text-4xl className  font-bold'>Career Site by React App</h2>
+                                    <div>
+                                        <a href="https://github.com/jasaiful/career_center_client.git/" target="_blank" rel="noopener noreferrer">
+                                            <button className="btn btn-ghost hover:text-white outline outline-2 outline-rose-500 mr-5 text-lg hover:bg-rose-500">Code</button>
+                                        </a>
+
+                                        <a href="https://neon-baklava-ee1d7f.netlify.app/" target="_blank" rel="noopener noreferrer">
+                                            <button className="btn btn-ghost hover:text-white outline outline-2 outline-rose-500 text-lg hover:bg-rose-500">Live</button>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    {/* slide-5 */}
+                    <SwiperSlide>
+                        <div id="slide5" className="carousel-item relative px-6 md:px-1 w-full">
+                            <img className="w-full rounded-xl" src='https://i.ibb.co/5WCwqSv/Survey-Master.jpg' alt="project_img" />
+                            <div className="absolute rounded-xl flex items-center justify-end h-full hero-overlay bg-opacity-40">
+                                <div className='text-white space-y-7 text-right pr-12'>
+                                    <h2 className='text-4xl className  font-bold'>Dynamic Survey Site by React</h2>
+                                    <div>
+                                        <a href="https://github.com/jasaiful/survey-master-client.git/" target="_blank" rel="noopener noreferrer">
+                                            <button className="btn btn-ghost hover:text-white outline outline-2 outline-rose-500 mr-5 text-lg hover:bg-rose-500">Code</button>
+                                        </a>
+
+                                        <a href="https://surveymaster-96ecd.web.app/" target="_blank" rel="noopener noreferrer">
                                             <button className="btn btn-ghost hover:text-white outline outline-2 outline-rose-500 text-lg hover:bg-rose-500">Live</button>
                                         </a>
                                     </div>
